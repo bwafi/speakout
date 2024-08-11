61e34a3f75991453aa5f4eb923b3a0e3f93bea75
Syahroni Bawafi <syahronibawafi@gmail.com>
2024-08-11 12:23:10 +0700

    initial commit

---
+++ src/main/java/com/speakout/SpeakoutApplication.java
@@ -0,0 +1,13 @@
+package com.speakout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeakoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeakoutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/speakout/SpeakoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.speakout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeakoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
