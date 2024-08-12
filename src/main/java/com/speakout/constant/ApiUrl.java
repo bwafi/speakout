f821a675b3ce217946d86d57f5b999a23fa604b6
Syahroni Bawafi <syahronibawafi@gmail.com>
2024-08-12 08:32:47 +0700

    feat: Add AuthController register

--- src/main/java/com/speakout/constant/ApiUrl.java
+++ src/main/java/com/speakout/constant/ApiUrl.java
@@ -1,5 +1,5 @@
 package com.speakout.constant;
 
 public class ApiUrl {
-  private static final String REGISTER_USER_URL = "/api/vi/register";
+  public static final String REGISTER_URL = "/api/vi/register";
 }
---
+++ src/main/java/com/speakout/controller/AuthController.java
@@ -0,0 +1,29 @@
+package com.speakout.controller;
+
+import com.speakout.DTOs.request.RegisterRequest;
+import com.speakout.DTOs.response.RegisterResponse;
+import com.speakout.DTOs.response.WebResponse;
+import com.speakout.constant.ApiUrl;
+import com.speakout.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(ApiUrl.REGISTER_URL)
+public class AuthController {
+  
+  @Autowired
+  private AuthService authService;
+  
+  @PostMapping
+  public ResponseEntity<WebResponse<RegisterResponse>> register(@RequestBody RegisterRequest registerRequest) {
+    RegisterResponse response = authService.register(registerRequest);
+    
+    return ResponseEntity.status(HttpStatus.CREATED).body(WebResponse.success(HttpStatus.CREATED.value(), "Successfully registered", response));
+  }
+}
